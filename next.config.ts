91805fc19eb8532650997daa081e7b9c385a62dc
Shashank <shashankshekhar746@gmail.com>
2025-04-19 23:41:00 +0530

    Added-required-pictures

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    domains: ['localhost:3000', 'localhost:3001', 'localhost:3002','lh3.googleusercontent.com']
+  }
 };
 
 export default nextConfig;
